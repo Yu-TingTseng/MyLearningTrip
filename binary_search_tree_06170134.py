f813017f9f169b5707243d5d71c7d6cfd8fde0cb
Yu-TingTseng <55703577+Yu-TingTseng@users.noreply.github.com>
2019-11-22 01:53:28 +0800

    程式碼

---
+++ binary_search_tree_06170134.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Solution(object):
+    def insert(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode(inserted node)
+        """
+        if root.val == None: 
+            root=TreeNode(val)
+            return root 
+        
+        else: 
+            if val > root.val:
+                if root.right==None: 
+                    root.right=TreeNode(val) 
+                    return root.right 
+                
+                else:
+                    return Solution().insert(root.right,val) 
+ 
+            elif val<= root.val:
+                if root.left==None: 
+                    root.left=TreeNode(val) 
+                    return root.left
+                
+                else:                    
+                    return Solution().insert(root.left,val)
+                
+                
+    def delete(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: int
+        :rtype: TreeNode(the root of new completed binary search tree) (cannot search())
+        """
+        if Solution().search(root,target)==None:
+            return None
+        else:
+            target =root
+            if root.left==None and root.right==None:
+                return root==None
+            elif root.left==None and root.right:
+                root.right=a
+                root=b
+                root.right=b
+                root=a
+                return root.right==None
+            elif root.right==None and root.left:
+                root.left=a
+                root=b
+                root.left=b
+                root=a
+                return root.left==None
+            else:
+                return root==None
+        
+        
+    def search(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: int
+        :rtype: TreeNode(searched node)
+        """
+        if root.val == None: 
+            return None
+        
+        else: 
+            if target > root.val: 
+                if root.right==None:
+                    return None
+                
+                else:
+                    return Solution().search(root.right,target)
+                
+            elif target< root.val: 
+                if root.left==None: 
+                    return None
+                
+                else:                    
+                    return Solution().search(root.left,target)
+                
+            else:
+                return root 
+            
+            
+    def modify(self, root, target, new_val):
+        """
+        :type root: TreeNode
+        :type target: int
+        :type new_val: int
+        :rtype:TreeNode(the root of new completed binary search tree) (cannot search())
+        """
+        if Solution().search(root,target)==None:
+
+            return 
+        else:
+            return Solution().delete(root,target)
+
